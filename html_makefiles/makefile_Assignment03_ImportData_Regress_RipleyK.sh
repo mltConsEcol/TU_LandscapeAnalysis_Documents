3b882dc659584efbbb1b9124d1e5503599486065
Mike Treglia <mtreglia@gmail.com>
2016-02-01 15:38:48 -0600

    final version of assignment 3 makefile w/out pdf

---
+++ html_makefiles/makefile_Assignment03_ImportData_Regress_RipleyK.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+#This works okay Step by Step in linux VM with R and the latest Pandoc installed
+
+R -e "rmarkdown::render('../Assignments_md/Assignment03_DataImport_Regress_RipleysK.Rmd', 'html_document', output_file='../html_makefiles/Assignment03_DataImport_Regress_RipleysK_tmp.html')";
+
+cat html_head_Down1Dir.html Assignment03_DataImport_Regress_RipleysK_tmp.html html_foot.html > ../Assignments_web/Assignment03_DataImport_Regress_RipleysK.html;
+
+rm Assignment03_DataImport_Regress_RipleysK_tmp.html;
+
+#R -e "rmarkdown::render('../Assignments_md/Assignment03_DataImport_Regress_RipleysK.Rmd', 'pdf_document', output_file='../Assignments_web/Assignment03_DataImport_Regress_RipleysK.pdf')";
